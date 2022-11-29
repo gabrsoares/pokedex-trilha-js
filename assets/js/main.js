7828b8f3df74633ea1c26fc515a8ad9adef69b7b
Gabriel Soares <gabrsoares@outlook.com>
2022-11-29 19:21:45 -0300

    atualizando modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function loadPokemonItems(offset, limit) {
 
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type}">
+            <li onclick="openModal()" class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,9 +1,12 @@
 const modal = document.querySelector(".modal");
 const openModalBtn = document.querySelector(".pokemon")
 const closeModalBtn = document.querySelector(".close")
+const modalName = document.querySelector(".poke-name")
 
 const openModal = function () {
     modal.classList.remove("hidden")
+    modalName.innerHTML = `${pokemon.type}`
+
 }
 
 const closeModal = function () {
